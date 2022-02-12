ad91a14bae6bd3c96d4712c813fa4b3b2f0b2bd5
Jakub Meller <s16942@pjwstk.edu.pl>
2022-02-12 14:07:52 +0100

    First project

---
+++ Project1/lesson_1_run.py
@@ -0,0 +1,5 @@
+print("Witamy w generatorze nazw zespołów")
+city = input("Podaj miasto, w którym się urodziłeś\n")
+pet = input("Podaj imię swojego pupila\n")
+print("Nazwa twojego zespołu powinna brzmieć: " + city + " " + pet)
+
