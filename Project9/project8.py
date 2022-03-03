d867fcb94c279139c55353ef4b680b26eea6ea1e
Jakub Meller - PC <kuba.meller@gmail.com>
2022-03-03 18:03:54 +0100

    Project 9

---
+++ Project9/project8.py
@@ -0,0 +1,20 @@
+players = {}
+loop = True
+
+while (loop):
+    name = input("Podaj swoje imie: ")
+    bid = int(input("Ile zł oferujesz za ten przedmiot: "))
+    players[name] = bid
+    decision = input("Czy są jeszcze inni gracze? (tak/t | nie/n)").lower()
+    if decision == "nie" or decision == "n":
+        loop = False
+
+maxV = 0
+winner = ""
+
+for value in players:
+    if players[value] > maxV:
+        maxV = players[value]
+        winner = value
+
+print(f"Zwycięzcą zostaje {winner}, który zapłaci {maxV}zł. Gratulacje!")
